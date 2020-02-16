b0d9b7f0bc91dc4c3e9367b312d1b40c46b44084
Andreas Schujkow <mail@andreasschujkow.de>
2020-02-16 21:06:07 +0100

    added more products

--- src/config/gen/products.js
+++ src/config/gen/products.js
@@ -8,23 +8,37 @@ export default [
   },
   {
     id: '1235132421',
-    image: 'https://i.picsum.photos/id/930/500/500.jpg',
+    image: 'https://i.picsum.photos/id/741/500/500.jpg',
     price: 55.95,
-    title: 'Montain View',
+    title: 'Cloudy Day',
     description: 'Consectetur laboriosam optio saepe ex perspiciatis assumenda eos. In illo aut hic id deserunt repellendus voluptatem labore earum. Occaecati dolorem quos consequuntur commodi non qui quia. Dignissimos voluptas optio autem ea voluptas.'
   },
   {
     id: '632434',
-    image: 'https://i.picsum.photos/id/930/500/500.jpg',
+    image: 'https://i.picsum.photos/id/95/500/500.jpg',
     price: 124.53,
-    title: 'Montain View',
+    title: 'Fog',
     description: 'Consectetur laboriosam optio saepe ex perspiciatis assumenda eos. In illo aut hic id deserunt repellendus voluptatem labore earum. Occaecati dolorem quos consequuntur commodi non qui quia. Dignissimos voluptas optio autem ea voluptas.'
   },
   {
     id: '6532133',
-    image: 'https://i.picsum.photos/id/930/500/500.jpg',
-    price: 61.22,
-    title: 'Montain View',
+    image: 'https://i.picsum.photos/id/176/500/500.jpg',
+    price: 1512.22,
+    title: 'Sea',
+    description: 'Consectetur laboriosam optio saepe ex perspiciatis assumenda eos. In illo aut hic id deserunt repellendus voluptatem labore earum. Occaecati dolorem quos consequuntur commodi non qui quia. Dignissimos voluptas optio autem ea voluptas.'
+  },
+  {
+    id: '84351',
+    image: 'https://i.picsum.photos/id/424/500/500.jpg',
+    price: 312.22,
+    title: 'House',
+    description: 'Consectetur laboriosam optio saepe ex perspiciatis assumenda eos. In illo aut hic id deserunt repellendus voluptatem labore earum. Occaecati dolorem quos consequuntur commodi non qui quia. Dignissimos voluptas optio autem ea voluptas.'
+  },
+  {
+    id: '8415112351',
+    image: 'https://i.picsum.photos/id/204/500/500.jpg',
+    price: 22.22,
+    title: 'Train',
     description: 'Consectetur laboriosam optio saepe ex perspiciatis assumenda eos. In illo aut hic id deserunt repellendus voluptatem labore earum. Occaecati dolorem quos consequuntur commodi non qui quia. Dignissimos voluptas optio autem ea voluptas.'
   },
 ]
