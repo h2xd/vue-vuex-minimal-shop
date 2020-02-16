bbcc5903db1023a8e3c86c96fae15d475a6ec4e7
Andreas Schujkow <mail@andreasschujkow.de>
2020-02-16 21:06:41 +0100

    product detail page

---
+++ src/pages/Product.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="page-content">
+    <h2 class="page-title">
+      {{ $route.params.id }}
+    </h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Product',
+    data() {
+      return {
+        title: 'Product',
+      };
+    },
+  };
+</script>
