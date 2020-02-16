9631e97efcfba9b9209d2c894a1a968caad0f9e1
Andreas Schujkow <mail@andreasschujkow.de>
2020-02-16 19:35:44 +0100

    added product listing

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="page-content">
-    <h2 class="page-title">
-      {{ title }}
-    </h2>
+    <ul class="product-listing">
+      <li
+        v-for="product in products"
+        :key="product.id">
+
+        <img :src="product.image">
+        <div>{{product.title}}</div>
+        <p>{{product.description}}</p>
+        <span>{{product.price}}</span>
+      </li>
+    </ul>
   </div>
+
 </template>
 
+<style lang="scss">
+  .product-listing {
+    list-style-type: none;
+
+    > li {
+      margin-bottom: 1rem;
+
+      img {
+        display: block;
+        width: 100%;
+        height: auto;
+      }
+    }
+  }
+
+</style>
+
 <script>
+  import { mapState } from 'vuex'
+
   export default {
     name: 'Index',
-    data() {
-      return {
-        title: 'Index page !',
-      };
-    },
+
+    computed: mapState({
+      products: state => state.products
+    }),
   };
 </script>
