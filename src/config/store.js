b7f92622cd2de858d0e229eed4165514c4d33222
Andreas Schujkow <mail@andreasschujkow.de>
2020-02-16 22:22:38 +0100

    added cart functionality

--- src/config/store.js
+++ src/config/store.js
@@ -6,13 +6,44 @@ const storeConfig = {
     getProducts: state => state.products,
     getProductById: (state) => (id) => {
       return state.products.find(product => product.id === id)
-    }
+    },
+    search: (state) => (search) => {
+      return state.products.find(product => product.title.toLowerCase().search(search.toLowerCase()) > 0)
+    },
+    getCart: state => {
+      return state.cart.map(item => {
+        return {
+          product: state.products.find(product => product.id === item[0]),
+          amount: item[1]
+        }
+      });
+    },
+    getCartAmount: state => state.cart.length
   },
   state: {
     products,
+    cart: []
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    addToCart(state, payload) {
+      const { id, amount } = payload;
+
+      let productFound = false;
+
+      state.cart = state.cart.map(item => {
+        if (item[0] === id) {
+          productFound = true;
+          return [item[0], item[1] + amount]
+        } else {
+          return [item[0], amount]
+        }
+      });
+
+      if (!productFound) {
+        state.cart.push([id, amount]);
+      }
+    }
+  },
 };
 
 export default storeConfig;
