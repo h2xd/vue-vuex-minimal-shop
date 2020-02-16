0190e1f6d5a29787def600162c5f31ed5af8a741
Andreas Schujkow <mail@andreasschujkow.de>
2020-02-16 22:21:33 +0100

    added generic footer details

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,17 +1,47 @@
 <template>
   <footer>
-    <p>shopinfo</p>
+    <table class="footer-details">
+      <tbody>
+        <tr>
+          <td>Telephone</td>
+          <td>(088) 252-5126 x7962</td>
+        </tr>
+        <tr>
+          <td>eMail</td>
+          <td>info@imgmeer.com</td>
+        </tr>
+        <tr>
+          <td>Address</td>
+          <td>
+            Block Run 21<br>
+            52450 South Mckayla<br>
+            Borders
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </footer>
 </template>
 
 <style lang="scss">
-  
-</style>
+  @import "../scss/main.scss";
 
-<script>
-  export default {
-    name: 'Footer',
-    data() {
-    },
-  };
-</script>
+  .footer-details {
+    width: calc(100% + 2rem);
+    color: var(--color-text);
+    vertical-align: top;
+    background-color: var(--color-background);
+    padding: 1rem;
+    margin: 3rem -1rem 0;
+
+    @include for-medium-screens {
+      margin: 3rem 0 0; 
+      width: 100%;
+    }
+
+    td {
+      padding: 0 0 5px;
+    }
+
+  }
+</style>
