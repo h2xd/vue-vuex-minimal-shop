536f0a8995de690cf0ac249143817c7121bcb61a
Andreas Schujkow <mail@andreasschujkow.de>
2020-02-16 23:11:08 +0100

    added basic product listing design

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -3,13 +3,21 @@
     <h2 class="page-title">
       {{ title }}
     </h2>
-    <ul class="product-listing" v-if="amount > 0">
-      <li
-        v-for="product in items"
-        :key="product.product.id">
-        <product :id="product.product.id" />
-      </li>
-    </ul>
+    <div v-if="amount > 0">
+      <div class="items">
+        <div v-for="item in items" class="item-cart-grid">
+          <img :src="item.product.image">
+          <div class="item-cart-grid-title">{{item.product.title}}</div>
+
+          <div class="item-cart-grid-amount">{{item.amount}}</div>
+          <div class="item-cart-grid-price">{{item.singlePrice}}</div>
+          <div class="item-cart-grid-total">{{item.totalPrice}}</div>
+          <div class="item-cart-grid-actions"></div>
+        </div>
+      </div>
+
+      <div class="cart-totalprice">Total: {{total}}</div>
+    </div>
     <router-link v-else class="go-shopping" :to="{ name: 'Home' }">
       empty cart :(<br>
       Let's go shopping
@@ -18,10 +26,68 @@
 </template>
 
 <style lang="scss">
+  .item-cart-grid {
+    display: grid;
+    grid-template-areas:
+      "image . title  title   title"
+      "image . amount price   total"
+      "image . .      actions actions";
+
+    grid-template-columns: minmax(50px, 75px) 1rem 60px 1fr 1fr;
+    margin-bottom: 1rem;
+
+    > img {
+      grid-area: image;
+      display: block;
+      width: 100%;
+      height: auto;
+      border-radius: 3px;
+    }
+  }
+
+  %label {
+    display: block;
+    font-size: 0.6rem;
+    text-transform: uppercase;
+    margin-bottom: 3px;
+    font-weight: bold;
+  }
+
+  @mixin label($text) {
+    &::before {
+      @extend %label;
+      content: "#{$text}";
+    }
+  }
+
+  .item-cart-grid-title {
+    grid-area: title;
+  }
+
+  .item-cart-grid-amount {
+    grid-area: amount;
+    @include label('Amount');
+  }
+
+  .item-cart-grid-price {
+    grid-area: price;
+    @include label('Price');
+  }
+
+  .item-cart-grid-total {
+    grid-area: total;
+    @include label('Sum');
+  }
+
+  .item-cart-grid-actions {
+    grid-area: actions;
+  }
+
+
   .page-title {
     margin-bottom: 20px;
   }
-  
+
   .go-shopping {
     padding: 1rem;
     border-radius: 3px;
@@ -32,22 +98,26 @@
     text-decoration: none;
     text-align: center;
   }
+
+  .cart-totalprice {
+    text-align: right;
+  }
 </style>
 
-<script>
-  import ProductCard from '../components/Product.vue';
-    
+<script>    
+  import formatPrice from '../utils/formatPrice';
+
   export default {
     name: 'Cart',
-    components: {
-      product: ProductCard
-    },
     computed: {
       amount() {
-        return this.$store.getters.getCartAmount.toString()
+        return this.$store.getters.getCartAmount.toString();
       },
       items() {
-        return this.$store.getters.getCart
+        return this.$store.getters.getCart;
+      },
+      total() {
+        return formatPrice(this.$store.getters.getTotalCartAmount);
       }
     },
     data() {
