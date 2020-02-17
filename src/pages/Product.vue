8182c3ac57e279c1d507480152afba0677972be0
Andreas Schujkow <mail@andreasschujkow.de>
2020-02-17 22:00:52 +0100

    added basic product page layout

--- src/pages/Product.vue
+++ src/pages/Product.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="page-content">
     <h2 class="page-title">
-      {{ $route.params.id }}
+      {{ product.title }}
     </h2>
+
+    <img :src="product.image">
+
+    <p class="product-description">{{product.description}}</p>
+
+    <button class="product-order-btn" @click="addToCart">Add to cart</button>
+    <div>
+      {{price}}
+    </div>
   </div>
 </template>
 
 <script>
+  import formatPrice from '../utils/formatPrice.js';
+
   export default {
     name: 'Product',
-    data() {
-      return {
-        title: 'Product',
-      };
+    computed: {
+      product() {
+        return this.$store.getters.getProductById(this.$route.params.id)
+      },
+      price() {
+        return formatPrice(this.$store.getters.getProductById(this.$route.params.id).price);
+      }
     },
+    methods: {
+      addToCart() {
+        this.$store.commit('addToCart', {
+          id: this.id,
+          amount: 1
+        });
+      }
+    }
   };
 </script>
